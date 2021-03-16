67debf6bba9c34931ac030de7d9f0392699cdaa5
Mark <mark.dubkov@gmail.com>
2021-03-16 20:59:06 +0700

    programmatically changing the view

--- interface-builder-task/LoginViewController.swift
+++ interface-builder-task/LoginViewController.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loginFieldBackgroundView: UIView!
+    @IBOutlet weak var passwordFieldBackgroundView: UIView!
+    
+    func setViewProperty(view v : UIView, radius r: CGFloat, width w: CGFloat) {
+        v.layer.cornerRadius = r
+        v.layer.borderWidth = w
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setViewProperty(view: loginFieldBackgroundView, radius: 8, width: 1)
+        setViewProperty(view: passwordFieldBackgroundView, radius: 8, width: 1)
         
     }
-
-
+    
 }
 
