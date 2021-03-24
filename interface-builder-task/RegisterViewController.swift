0d1ef9eebf4f2ff55369bfcf6d4dc88c6d7ca99a
Mark <mark.dubkov@gmail.com>
2021-03-24 12:31:51 +0700

    code and view refactoring

--- interface-builder-task/RegisterViewController.swift
+++ interface-builder-task/RegisterViewController.swift
@@ -15,9 +15,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordRegisterTextField: CustomTextField!
     
     override func viewDidLoad() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
+        registerKeyboardNotifications()
         hideKeyboardWhenTappedAround()
         reDelegateTextFields()
     }
@@ -26,19 +24,6 @@ class RegisterViewController: UIViewController {
         loginRegisterTextField.delegate = self
         passwordRegisterTextField.delegate = self
         emailRegisterTextField.delegate = self
-    }
-    
-    @objc func keyboardWillShow(sender: Notification) {
-        if let keyboardSize = (sender.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            let keyboardHeight = keyboardSize.height
-            var inset = scrollView.contentInset
-            inset.bottom = keyboardHeight - view.safeAreaInsets.bottom
-            scrollView.contentInset = inset
-        }
-    }
-    
-    @objc func keyboardWillHide(sender: Notification) {
-        scrollView.contentInset = .zero
     }
     
     private func hideKeyboardWhenTappedAround() {
@@ -51,9 +36,12 @@ class RegisterViewController: UIViewController {
     }
     
     deinit {
-        NotificationCenter.default.removeObserver(self)
+        unregisterKeyboardNotifications()
     }
 }
+
+// MARK: Scroll View Keyboard Handler
+extension RegisterViewController : ScrollViewKeyboardDelegate {}
 
 extension RegisterViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
